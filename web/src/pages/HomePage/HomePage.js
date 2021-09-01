c30680105ee2249e7435e5b1e0feed1d6d4e40f3
Geoff Davis <geoff@email2davis.co.uk>
2021-09-02 00:48:59 +0100

    Ready to deploy to netlify

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import { MetaTags } from '@redwoodjs/web'
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags
+        title="Home"
+        // description="Home description"
+        /* you should un-comment description and add a unique description, 155 characters or less
+      You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
+      />
+
+      <main>
+        <BlogPostsCell />
+      </main>
+    </>
+  )
+}
+
+export default HomePage
